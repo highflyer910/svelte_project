6336a14732bfb0bb398d512d98d66d1b7e8d7af4
Thea <thea.mushambadze@gmail.com>
2020-05-10 20:55:14 +0400

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,24 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import CreatePollForm from './components/CreatePollForm.svelte';
   import Tabs from './shared/Tabs.svelte';
 
   let items = ['Current Polls', "Add New Poll"];
   let activeItem = "Current Polls";
+  const tabChange = (e) => {
+     activeItem = e.detail;
+  }
 </script>
 
 <Header />
 <main>
-  <Tabs {activeItem} {items}/>
+  <Tabs {activeItem} {items} on:tabChange={tabChange}/>
+  {#if activeItem === 'Current Polls'}
+     <p>poll components goes here</p>
+  {:else if activeItem === 'Add New Poll'}
+     <CreatePollForm />
+  {/if}     
 </main>
 <Footer />
 
--- src/shared/Tabs.svelte
+++ src/shared/Tabs.svelte
@@ -1,4 +1,7 @@
 <script>
+    import {createEventDispatcher} from 'svelte';
+    const dispatch = createEventDispatcher();
+
 	export let items;
 	export let activeItem;
 </script>
@@ -7,7 +10,7 @@
 <div class="tabs">
    <ul>
     {#each items as item}
-      <li>
+      <li on:click={() => dispatch('tabChange', item)}>
       	<div class:active = {item === activeItem}>{item}</div>
       </li>
     {/each}
@@ -16,8 +19,24 @@
 
 
 <style>
+	.tabs{
+		margin-bottom: 40px;
+	}
+	ul{
+	    display: flex;
+	    justify-content: center;
+	    padding: 0;
+	    list-style-type: none;
+	}
+	li{
+		margin: 0 16px;
+	    font-size: 18px;
+	    color: #555;
+	    cursor: pointer;
+	}
    .active{
    	color: darkred;
-   	border-color: 2px solid darkred;
+   	border-bottom: 2px solid darkred;
+
    }
 </style>
